5dd087cf9bb351ffcf688eab92cef2a104c1afa8
Chas Berndt <chas.berndt@gmail.com>
2022-04-11 11:13:56 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+from pathlib import Path
+from shutil import move
+import logging
+import configparser
+import glob
+
+
+def create_directories(base_directory, jpeg_directory, raw_directory, edits_directory):
+    directories_to_create = list()
+
+    directories_to_create.append(os.path.join(base_directory, jpeg_directory))
+    directories_to_create.append(os.path.join(base_directory, raw_directory))
+    directories_to_create.append(os.path.join(base_directory, edits_directory))
+
+    for directory in directories_to_create:
+        if not os.path.isdir(directory):
+            print(f'Creating {directory}')
+            os.mkdir(directory)
+
+
+def sort_files(base_directory, jpeg_directory, jpeg_extension, raw_directory, raw_extension):
+    jpegs = glob.glob(os.path.join(base_directory, f'*{jpeg_extension}'))
+    for jpeg in jpegs:
+        move(jpeg, jpeg_directory)
+
+    raws = glob.glob(os.path.join(base_directory, f'*{raw_extension}'))
+    for raw in raws:
+        move(raw, raw_directory)
+
+
+def delete_files(base_directory, jpeg_directory, raw_directory, ignore_hidden_files):
+    approved_jpegs = list()
+
+    for base, dirs, files in os.walk(os.path.join(base_directory, jpeg_directory)):
+        for file in files:
+            if ignore_hidden_files:
+                if not file.startswith('.'):
+                    approved_jpegs.append(Path(file).stem)
+            else:
+                approved_jpegs.append(Path(file).stem)
+
+    for base, dirs, files in os.walk(os.path.join(base_directory, raw_directory)):
+        for file in files:
+            full_file_path = os.path.join(base, file)
+            if Path(file).stem in approved_jpegs:
+                print(f'Leaving {full_file_path}')
+            else:
+                print(f'Deleting {full_file_path}')
+                os.remove(full_file_path)
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--photo_directory', '-p')
+    parser.add_argument('--config_file', '-c')
+    args = parser.parse_args()
+
+    photoflow_config = configparser.ConfigParser()
+    photoflow_config.read(args.config_file)
+    jpeg_directory = photoflow_config["basic_configuration"]["jpeg_directory"]
+    raw_directory = photoflow_config["basic_configuration"]["raw_directory"]
+    edits_directory = photoflow_config["basic_configuration"]["edits_directory"]
+    jpeg_file_extension = photoflow_config["basic_configuration"]["jpeg_file_extension"]
+    raw_file_extension = photoflow_config["basic_configuration"]["raw_file_extension"]
+    ignore_hidden_files = bool(photoflow_config["basic_configuration"]["ignore_hidden_files"])
+
+    create_directories(args.photo_directory, jpeg_directory, raw_directory, edits_directory)
+
+    sort_files(args.photo_directory, jpeg_directory, jpeg_file_extension, raw_directory, raw_file_extension)
+
+    delete_files(args.photo_directory, jpeg_directory, raw_directory, ignore_hidden_files)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
